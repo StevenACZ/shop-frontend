8044301a0c9e43f63eea16aa0d29eff1d114abaf
stevenacz <scoailazaa@gmail.com>
2021-03-18 11:53:00 -0500

    basic css added

---
+++ src/components/layout/layout/Styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { color } from '../../../theme/variables';
+
+export const Container = styled.main`
+  max-width: 1000px;
+  margin: 30px auto;
+  background-color: ${color.colorWhite};
+`;
