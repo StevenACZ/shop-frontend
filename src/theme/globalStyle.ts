64c231d977bf128af146db4897843df9d734c9f9
stevenacz <scoailazaa@gmail.com>
2021-03-18 11:54:44 -0500

    basic global styles added

---
+++ src/theme/globalStyle.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: Arial, Helvetica, sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+export default GlobalStyle;
