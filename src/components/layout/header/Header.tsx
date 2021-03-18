15f5620dc2231631c72f97ad3c68a243e3627c2d
stevenacz <scoailazaa@gmail.com>
2021-03-18 11:52:15 -0500

    Header component added

---
+++ src/components/layout/header/Header.tsx
@@ -0,0 +1,48 @@
+// React
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Button from '../../button/Button';
+
+// Styles
+import {
+  HeaderStyled,
+  Logo,
+  Search,
+  Navbar,
+  Links,
+  Profile,
+  HeaderContainer,
+} from './Styles';
+
+interface Props {}
+
+const Header: React.FC<Props> = () => {
+  return (
+    <HeaderStyled>
+      <HeaderContainer>
+        <Logo>
+          <NavLink to="/">
+            <h2>Awesome Shop</h2>
+          </NavLink>
+        </Logo>
+
+        <Search>
+          <label>
+            <input type="text" placeholder="Search" />
+          </label>
+          <Button>Search</Button>
+        </Search>
+
+        <Navbar>
+          <Links></Links>
+          <Profile>
+            <Button>Sign in</Button>
+            <Button>Sign up</Button>
+          </Profile>
+        </Navbar>
+      </HeaderContainer>
+    </HeaderStyled>
+  );
+};
+
+export default Header;
