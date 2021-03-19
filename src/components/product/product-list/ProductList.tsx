9d614cc8ae1e6daca0eb6e5c6abdf3378c17fccc
stevenacz <scoailazaa@gmail.com>
2021-03-19 13:52:18 -0500

    fetch to api

--- src/components/product/product-list/ProductList.tsx
+++ src/components/product/product-list/ProductList.tsx
@@ -1,23 +1,45 @@
 // React
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+// Axios
+import axios from '../../../axios';
 
 // Styles
 import { ProductListStyled } from './Styles';
 
 // Types
-import products from '../../../data/products';
+import { Product } from '../../../data/products';
 
 // Components
 import ProductListItem from './product-list-item/ProductListItem';
+import Spinner from '../../spinner/Spinner';
 
 interface Props {}
 
 const ProductList: React.FC<Props> = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await axios.get('/api/products');
+
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <ProductListStyled>
-      {products.map((product) => (
-        <ProductListItem key={product._id} {...product} />
-      ))}
+      {products.length > 0 ? (
+        <>
+          {products.map((product: Product) => (
+            <ProductListItem key={product._id} {...product} />
+          ))}
+        </>
+      ) : (
+        <Spinner height={50} width={50} />
+      )}
     </ProductListStyled>
   );
 };
