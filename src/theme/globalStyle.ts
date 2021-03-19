f67320c842167c3cd9c719dca773e70969d64a3b
stevenacz <scoailazaa@gmail.com>
2021-03-18 22:25:37 -0500

    reset to h1,h2,h3...

--- src/theme/globalStyle.ts
+++ src/theme/globalStyle.ts
@@ -3,16 +3,23 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   * {
     font-family: Arial, Helvetica, sans-serif;
-    margin: 0;
-    padding: 0;
+    margin: 0px;
+    padding: 0px;
     box-sizing: border-box;
-  }
-
-  body {
   }
 
   a {
     text-decoration: none;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin: 0px;
   }
 `;
 
