6d7b1e22384aa720c5a393750b2e69556c5e8661
stevenacz <scoailazaa@gmail.com>
2021-03-19 12:50:05 -0500

    improved

--- src/components/product/product-details/Styles.ts
+++ src/components/product/product-details/Styles.ts
@@ -3,21 +3,29 @@ import styled from 'styled-components';
 export const ProductDetailsStyled = styled.section``;
 
 export const Header = styled.div`
-  border: 1px solid black;
-  margin-bottom: 10px;
+  margin-bottom: 15px;
 `;
 
 export const Main = styled.div`
   display: grid;
-  grid-template-columns: 40% 1fr 1fr;
-  column-gap: 20px;
-  border: 1px solid red;
+  grid-template-columns: 35% 1fr 1fr;
+  column-gap: 40px;
+
+  @media (max-width: 1000px) {
+  }
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    column-gap: 0;
+    row-gap: 30px;
+  }
 `;
 
 export const Image = styled.div`
   & > img {
     width: 100%;
     height: 100%;
+    object-fit: cover;
   }
 `;
 
@@ -38,8 +46,12 @@ export const Description = styled.div`
     display: flex;
     align-items: center;
 
+    & > ul {
+      font-size: 15px;
+    }
+
     & > p {
-      margin-left: 10px;
+      margin-left: 15px;
     }
   }
 
@@ -54,5 +66,35 @@ export const Description = styled.div`
 `;
 
 export const AddToCart = styled.div`
-  border: 1px solid green;
+  & > div {
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  & > div:nth-child(1) {
+    border-top: 1px solid black;
+    border-left: 1px solid black;
+    border-right: 1px solid black;
+    border-bottom: 1px solid black;
+  }
+
+  & > div:nth-child(2) {
+    border-left: 1px solid black;
+    border-right: 1px solid black;
+    border-bottom: 1px solid black;
+  }
+
+  & > div:nth-child(3) {
+    border-left: 1px solid black;
+    border-right: 1px solid black;
+    border-bottom: 1px solid black;
+  }
+
+  & > div:nth-child(4) {
+    border-left: 1px solid black;
+    border-right: 1px solid black;
+    border-bottom: 1px solid black;
+  }
 `;
