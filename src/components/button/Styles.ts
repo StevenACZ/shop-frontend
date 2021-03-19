2a57408d2fc63948041ba5a8e7743ac53cf5eed1
stevenacz <scoailazaa@gmail.com>
2021-03-19 12:51:32 -0500

    improved

--- src/components/button/Styles.ts
+++ src/components/button/Styles.ts
@@ -11,10 +11,8 @@ export const ButtonStyled = styled.button<Props>`
   width: ${(p) => p.width};
   height: ${(p) => p.height};
   cursor: ${(p) => (p.loading ? 'not-allowed' : 'pointer')};
-  background-color: ${color.colorPrimary};
-  border-radius: 4px;
+  background-color: ${color.colorBlack};
   color: ${color.colorWhite};
   border: none;
   outline: none;
-  transition: 400ms ease-out;
 `;
