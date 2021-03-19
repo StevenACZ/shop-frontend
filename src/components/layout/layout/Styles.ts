2ab88afc935f10b6a3a95c1f59b6a6403db533f8
stevenacz <scoailazaa@gmail.com>
2021-03-19 11:12:16 -0500

    responsive added

--- src/components/layout/layout/Styles.ts
+++ src/components/layout/layout/Styles.ts
@@ -5,4 +5,8 @@ export const Container = styled.main`
   max-width: 1000px;
   margin: 30px auto;
   background-color: ${color.colorWhite};
+
+  @media (max-width: 1000px) {
+    padding: 0 20px;
+  }
 `;
