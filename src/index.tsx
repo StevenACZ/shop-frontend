aee5ca25ac94ac9d5f2de4329b968c003a76a9fc
stevenacz <scoailazaa@gmail.com>
2021-03-23 17:54:38 -0500

    redux added

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store/configureStore';
+
 // Components
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
