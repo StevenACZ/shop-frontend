47c96618a4115fc76742909ae86a76faadc87248
stevenacz <scoailazaa@gmail.com>
2021-03-24 11:26:26 -0500

    added

---
+++ src/actions/productList.ts
@@ -0,0 +1,27 @@
+// Axios
+import axios from '../axios/index';
+
+// Redux - Slices
+import {
+  productListRequest,
+  productListSuccess,
+  productListFail,
+} from '../slices/productList';
+
+export const listProducts = () => async (dispatch: any) => {
+  try {
+    dispatch(productListRequest());
+
+    const { data } = await axios.get('/api/products');
+
+    dispatch(productListSuccess(data));
+  } catch (error) {
+    dispatch(
+      productListFail(
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      )
+    );
+  }
+};
