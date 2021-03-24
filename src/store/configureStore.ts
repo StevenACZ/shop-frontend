93fc8b919540d0bb7555ba5f87bad76482cc8a5c
stevenacz <scoailazaa@gmail.com>
2021-03-24 16:15:50 -0500

    added

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -4,11 +4,13 @@ import { configureStore } from '@reduxjs/toolkit';
 // Redux - Slices
 import { productListSlice } from '../slices/productList';
 import { productDetailsSlice } from '../slices/productDetails';
+import { cartSlice } from '../slices/cart';
 
 const store = configureStore({
   reducer: {
     productList: productListSlice.reducer,
     productDetails: productDetailsSlice.reducer,
+    cart: cartSlice.reducer,
   },
 });
 
