faf22aba028f90f1c47e3cbe30632086c1f81fec
stevenacz <scoailazaa@gmail.com>
2021-03-24 11:27:38 -0500

    added

---
+++ src/slices/productDetails.ts
@@ -0,0 +1,70 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { RootState } from '../store/configureStore';
+
+interface Product {
+  _id: string;
+  name: string;
+  image: string;
+  description: string;
+  brand: string;
+  category: string;
+  price: number;
+  countInStock: number;
+  rating: number;
+  numReviews: number;
+  reviews: [];
+}
+
+interface ProductDetailsState {
+  product: Product;
+  loading: boolean;
+  error: string;
+}
+
+const initialState: ProductDetailsState = {
+  product: {
+    _id: '',
+    name: '',
+    image: '',
+    description: '',
+    brand: '',
+    category: '',
+    price: 0,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+    reviews: [],
+  },
+  loading: true,
+  error: '',
+};
+
+export const productDetailsSlice = createSlice({
+  name: 'productDetails',
+  initialState,
+  reducers: {
+    productRequest: (state) => {
+      state.loading = true;
+    },
+    productSuccess: (state, action) => {
+      state.product = action.payload;
+      state.loading = false;
+    },
+    productFail: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+  },
+});
+
+export const {
+  productRequest,
+  productSuccess,
+  productFail,
+} = productDetailsSlice.actions;
+
+export const selectProduct = (state: RootState) => state.productDetails.product;
+export const selectLoading = (state: RootState) => state.productDetails.loading;
+export const selectError = (state: RootState) => state.productDetails.error;
+
+export default productDetailsSlice.reducer;
