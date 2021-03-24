15c24151f247951abbcb516b0df5b23e650339bd
stevenacz <scoailazaa@gmail.com>
2021-03-24 16:15:10 -0500

    redux added

--- src/pages/cart/CartScreen.tsx
+++ src/pages/cart/CartScreen.tsx
@@ -1,9 +1,31 @@
 // React
-import React from 'react';
+import React, { useEffect } from 'react';
+
+// Redux
+import { useDispatch } from 'react-redux';
+
+// Redux - Actions
+import { addToCart } from '../../actions/cart';
+
+// React Router
+import { useLocation, useParams } from 'react-router';
 
 interface Props {}
 
 const CartScreen: React.FC<Props> = () => {
+  const dispatch = useDispatch();
+
+  const { productID } = useParams() as { productID: string };
+  const location = useLocation();
+
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
+  useEffect(() => {
+    if (productID) {
+      dispatch(addToCart(productID, qty));
+    }
+  }, [dispatch, productID, qty]);
+
   return <div>CartScreen</div>;
 };
 
