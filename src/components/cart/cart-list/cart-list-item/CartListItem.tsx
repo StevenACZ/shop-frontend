708f782f610b895e80f7626c079be5e92ae2b378
stevenacz <scoailazaa@gmail.com>
2021-03-25 10:29:59 -0500

    added

---
+++ src/components/cart/cart-list/cart-list-item/CartListItem.tsx
@@ -0,0 +1,13 @@
+// React
+import React from 'react';
+
+// Styles
+import { CartListItemStyled } from './Styles';
+
+interface Props {}
+
+const CartListItem: React.FC<Props> = () => {
+  return <CartListItemStyled>CartListItem</CartListItemStyled>;
+};
+
+export default CartListItem;
