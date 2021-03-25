56e2c9ab932319b9fbf2e8811adb7856debaabf8
stevenacz <scoailazaa@gmail.com>
2021-03-25 11:42:37 -0500

    improved

--- src/pages/cart/Styles.ts
+++ src/pages/cart/Styles.ts
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
 
 export const CartScreenStyled = styled.section`
+  display: grid;
+  grid-template-columns: 60% 1fr;
+  grid-column-gap: 40px;
+`;
+
+export const Left = styled.div`
   & > h2 {
     margin-bottom: 20px;
     text-transform: uppercase;
   }
 `;
+
+export const Right = styled.div``;
