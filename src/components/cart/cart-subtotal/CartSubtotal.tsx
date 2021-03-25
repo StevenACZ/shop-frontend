485968edde7efff540477896e73187f706df71a1
stevenacz <scoailazaa@gmail.com>
2021-03-25 11:42:07 -0500

    improved

--- src/components/cart/cart-subtotal/CartSubtotal.tsx
+++ src/components/cart/cart-subtotal/CartSubtotal.tsx
@@ -2,12 +2,26 @@
 import React from 'react';
 
 // Styles
-import { CartSubtotalStyled } from './Styles';
+import { CartSubtotalStyled, Summary, Checkout } from './Styles';
+
+// Component
+import Button from '../../button/Button';
 
 interface Props {}
 
 const CartSubtotal: React.FC<Props> = () => {
-  return <CartSubtotalStyled>CartSubtotal</CartSubtotalStyled>;
+  return (
+    <CartSubtotalStyled>
+      <Summary>
+        <h3>Subtotal (3) items</h3>
+        <p>$579.97</p>
+      </Summary>
+
+      <Checkout>
+        <Button width="100%">Proceed to checkout</Button>
+      </Checkout>
+    </CartSubtotalStyled>
+  );
 };
 
 export default CartSubtotal;
--- src/components/cart/cart-subtotal/Styles.ts
+++ src/components/cart/cart-subtotal/Styles.ts
@@ -1,8 +1,23 @@
 import styled from 'styled-components';
 
-export const CartSubtotalStyled = styled.section`
-  & > h2 {
-    margin-bottom: 20px;
+export const CartSubtotalStyled = styled.div`
+  border: 1px solid black;
+`;
+export const Summary = styled.div`
+  border-bottom: 1px solid black;
+  padding: 10px 20px;
+
+  & > h3 {
+    font-size: 25px;
+    text-transform: uppercase;
+    margin-bottom: 10px;
+  }
+`;
+
+export const Checkout = styled.div`
+  padding: 10px 20px;
+
+  & > button {
     text-transform: uppercase;
   }
 `;
