0e5b0ff3282be5b3e2b5cbcc44b5e0d6794580bf
stevenacz <scoailazaa@gmail.com>
2021-03-25 13:24:21 -0500

    CartSubtotal added

--- src/pages/cart/CartScreen.tsx
+++ src/pages/cart/CartScreen.tsx
@@ -8,13 +8,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addToCart } from '../../actions/cart';
 
 // Redux - Slices
-import { selectCartItems, selectError, selectLoading } from '../../slices/cart';
+import { selectCartItems } from '../../slices/cart';
 
 // React Router
-import { useLocation, useParams } from 'react-router';
+import { useHistory, useLocation, useParams } from 'react-router';
 
 // Styles
 import { CartScreenStyled, Left, Right } from './Styles';
+
+// Antd Components
+import { Alert, Button } from 'antd';
 
 // Components
 import CartList from '../../components/cart/cart-list/CartList';
@@ -23,16 +26,14 @@ import CartSubtotal from '../../components/cart/cart-subtotal/CartSubtotal';
 interface Props {}
 
 const CartScreen: React.FC<Props> = () => {
+  // History
+  const history = useHistory();
+
   // Dispatch
   const dispatch = useDispatch();
 
   // Selector
   const productList = useSelector(selectCartItems);
-  const loading = useSelector(selectLoading);
-  const error = useSelector(selectError);
-
-  console.log(loading);
-  console.log(error);
 
   const { productID } = useParams() as { productID: string };
   const location = useLocation();
@@ -45,15 +46,43 @@ const CartScreen: React.FC<Props> = () => {
     }
   }, [dispatch, productID, qty]);
 
+  const getQuantityItems = () =>
+    productList.reduce((acc, item: any) => acc + item.qty, 0);
+
+  const getTotalAmount = () =>
+    productList.reduce((acc, item: any) => acc + item.price * item.qty, 0);
+
   return (
     <CartScreenStyled>
       <Left>
         <h2>Shopping cart</h2>
-        <CartList productList={productList} />
+
+        {getQuantityItems() === 0 ? (
+          <Alert
+            message="Your cart is empty"
+            type="error"
+            showIcon
+            banner
+            action={
+              <Button
+                size="small"
+                type="primary"
+                onClick={() => history.push('/')}
+              >
+                Go Home
+              </Button>
+            }
+          />
+        ) : (
+          <CartList productList={productList} />
+        )}
       </Left>
 
       <Right>
-        <CartSubtotal />
+        <CartSubtotal
+          quantityItems={getQuantityItems()}
+          totalAmount={getTotalAmount()}
+        />
       </Right>
     </CartScreenStyled>
   );
