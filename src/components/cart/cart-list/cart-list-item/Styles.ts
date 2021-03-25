53cdd1fe86b08e67101fc4febc8cb936fa83f5af
stevenacz <scoailazaa@gmail.com>
2021-03-25 11:09:43 -0500

    improved

--- src/components/cart/cart-list/cart-list-item/Styles.ts
+++ src/components/cart/cart-list/cart-list-item/Styles.ts
@@ -1,3 +1,50 @@
 import styled from 'styled-components';
 
-export const CartListItemStyled = styled.section``;
+export const CartListItemStyled = styled.section`
+  display: grid;
+  grid-template-columns: 15% 1fr 10% 15% 15%;
+  grid-column-gap: 20px;
+
+  padding: 10px 0;
+`;
+
+export const Image = styled.div`
+  border-radius: 8px;
+  overflow: hidden;
+
+  width: 100%;
+  height: 100%;
+
+  & > img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+export const Name = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  & > h3 {
+    font-size: 14px;
+    line-height: 18px;
+  }
+`;
+
+export const Price = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Quantity = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Remove = styled.div`
+  border-radius: 8px;
+  overflow: hidden;
+`;
