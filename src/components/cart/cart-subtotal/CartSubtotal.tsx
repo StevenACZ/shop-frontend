e73cf7d5afa6110fa480a827d1671acf934e50d8
stevenacz <scoailazaa@gmail.com>
2021-03-25 11:15:32 -0500

    CartSubtotal added

---
+++ src/components/cart/cart-subtotal/CartSubtotal.tsx
@@ -0,0 +1,13 @@
+// React
+import React from 'react';
+
+// Styles
+import { CartSubtotalStyled } from './Styles';
+
+interface Props {}
+
+const CartSubtotal: React.FC<Props> = () => {
+  return <CartSubtotalStyled>CartSubtotal</CartSubtotalStyled>;
+};
+
+export default CartSubtotal;
