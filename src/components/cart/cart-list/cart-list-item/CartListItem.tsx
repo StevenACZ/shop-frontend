10e5047362681973601d4b2acd7f8bd84f3e1c2b
stevenacz <scoailazaa@gmail.com>
2021-03-25 11:09:37 -0500

    improved

--- src/components/cart/cart-list/cart-list-item/CartListItem.tsx
+++ src/components/cart/cart-list/cart-list-item/CartListItem.tsx
@@ -2,12 +2,69 @@
 import React from 'react';
 
 // Styles
-import { CartListItemStyled } from './Styles';
+import {
+  CartListItemStyled,
+  Image,
+  Name,
+  Price,
+  Quantity,
+  Remove,
+} from './Styles';
 
-interface Props {}
+// Antd Icons
+import { DeleteFilled } from '@ant-design/icons';
 
-const CartListItem: React.FC<Props> = () => {
-  return <CartListItemStyled>CartListItem</CartListItemStyled>;
+// Antd Components
+import { InputNumber } from 'antd';
+
+// Components
+import Button from '../../../button/Button';
+
+interface Props {
+  name: string;
+  image: string;
+  price: number;
+  countInStock: number;
+  qty: number;
+}
+
+const CartListItem: React.FC<Props> = ({
+  name,
+  image,
+  price,
+  countInStock,
+  qty,
+}) => {
+  return (
+    <CartListItemStyled>
+      <Image>
+        <img src={image} alt={name} />
+      </Image>
+
+      <Name>
+        <h3>{name}</h3>
+      </Name>
+
+      <Price>
+        <p>${price.toFixed(2)}</p>
+      </Price>
+
+      <Quantity>
+        <InputNumber
+          min={1}
+          max={countInStock}
+          defaultValue={qty}
+          // onChange={onChange}
+        />
+      </Quantity>
+
+      <Remove>
+        <Button width="100%" height="100%">
+          <DeleteFilled />
+        </Button>
+      </Remove>
+    </CartListItemStyled>
+  );
 };
 
 export default CartListItem;
