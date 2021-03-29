bc6bc3abd8d809c1df65f10f860f6d583156ecaf
stevenacz <scoailazaa@gmail.com>
2021-03-29 11:56:26 -0500

    user reducer added

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -5,12 +5,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import { productListSlice } from '../slices/productList';
 import { productDetailsSlice } from '../slices/productDetails';
 import { cartSlice } from '../slices/cart';
+import { userSlice } from '../slices/user';
 
 const store = configureStore({
   reducer: {
     productList: productListSlice.reducer,
     productDetails: productDetailsSlice.reducer,
     cart: cartSlice.reducer,
+    user: userSlice.reducer,
   },
 });
 
