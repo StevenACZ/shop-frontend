4754f8611e2382ca8d916b96177537ba38c3e512
stevenacz <scoailazaa@gmail.com>
2021-03-30 12:44:48 -0500

    Profile screen added

---
+++ src/pages/profile/ProfileScreen.tsx
@@ -0,0 +1,13 @@
+// React
+import React from 'react';
+
+// Styles
+import { ProfileScreenStyled } from './Styles';
+
+interface Props {}
+
+const ProfileScreen: React.FC<Props> = () => {
+  return <ProfileScreenStyled>Profile</ProfileScreenStyled>;
+};
+
+export default ProfileScreen;
