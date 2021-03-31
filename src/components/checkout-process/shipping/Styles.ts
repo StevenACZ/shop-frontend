68a1bb8c2a97019e5ad9e5691b9db164c204d6b0
stevenacz <scoailazaa@gmail.com>
2021-03-31 12:34:50 -0500

    added

---
+++ src/components/checkout-process/shipping/Styles.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const ShippingStyled = styled.section`
+  padding: 20px 0;
+`;
+
+export const Form = styled.form`
+  max-width: 40%;
+  margin: 0 auto;
+
+  & > h2 {
+    margin-bottom: 20px;
+    text-transform: uppercase;
+    font-weight: bold;
+  }
+
+  & > button {
+    margin-top: 15px;
+  }
+
+  @media (max-width: 1000px) {
+    max-width: 50%;
+  }
+
+  @media (max-width: 768px) {
+    max-width: 95%;
+  }
+`;
+
+export const GoLogin = styled.p`
+  margin-top: 20px;
+
+  & > span {
+    font-weight: bold;
+    cursor: pointer;
+    margin-left: 7px;
+  }
+
+  & > span:hover {
+    text-decoration: underline;
+  }
+`;
