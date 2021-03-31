80b096aa26f6d3f4dca5e66502a0b5f3c9ad55cd
stevenacz <scoailazaa@gmail.com>
2021-03-31 12:34:44 -0500

    added

---
+++ src/components/checkout-process/shipping/Shipping.tsx
@@ -0,0 +1,13 @@
+// React
+import React from 'react';
+
+// Styles
+import { ShippingStyled } from './Styles';
+
+interface Props {}
+
+const Shipping: React.FC<Props> = () => {
+  return <ShippingStyled>Shipping</ShippingStyled>;
+};
+
+export default Shipping;
