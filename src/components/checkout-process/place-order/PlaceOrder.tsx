e33d5e0a43109f599b2b11961956e1e06277963c
stevenacz <scoailazaa@gmail.com>
2021-03-31 22:48:06 -0500

    improved

--- src/components/checkout-process/place-order/PlaceOrder.tsx
+++ src/components/checkout-process/place-order/PlaceOrder.tsx
@@ -1,12 +1,11 @@
 // React
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // Redux
-// import { useDispatch, useSelector } from 'react-redux';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 // Redux - Actions
-// import { listProductDetails } from '../../actions/productDetails';
+import { createOrder } from '../../../actions/order';
 
 // Redux - Slices
 import {
@@ -14,6 +13,12 @@ import {
   selectPaymentMethod,
   selectShippingAddress,
 } from '../../../slices/cart';
+import {
+  selectError,
+  selectLoading,
+  selectOrders,
+  selectSuccess,
+} from '../../../slices/order';
 
 // React Router
 import { useHistory } from 'react-router';
@@ -30,6 +35,9 @@ import {
   OrderCant,
 } from './Styles';
 
+// Antd Components
+import { Alert, Spin } from 'antd';
+
 // Components
 import Button from '../../button/Button';
 
@@ -40,7 +48,7 @@ const PlaceOrder: React.FC<Props> = () => {
   const history = useHistory();
 
   // Dispatch
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   // Selector
   const products = useSelector(selectCartItems);
@@ -51,6 +59,18 @@ const PlaceOrder: React.FC<Props> = () => {
     country: string;
   };
   const paymentMethod = useSelector(selectPaymentMethod);
+  const order = useSelector(selectOrders) as {
+    _id: string;
+  };
+  const loading = useSelector(selectLoading);
+  const success = useSelector(selectSuccess);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    if (success) {
+      history.push(`/order/${order._id}`);
+    }
+  }, [history, success, order]);
 
   const itemsPrice = products.reduce(
     (acc, item: any) => acc + item.price * item.qty,
@@ -59,85 +79,118 @@ const PlaceOrder: React.FC<Props> = () => {
 
   const shippingPrice = itemsPrice > 100 ? 0 : 100;
   const taxPrice = Number((0.15 * itemsPrice).toFixed(2));
-  const totalPrice = (
-    Number(itemsPrice) +
-    Number(shippingPrice) +
-    Number(taxPrice)
-  ).toFixed(2);
+  const totalPrice = Number(
+    (Number(itemsPrice) + Number(shippingPrice) + Number(taxPrice)).toFixed(2)
+  );
+
+  const placeOrderHanlder = () => {
+    dispatch(
+      createOrder({
+        orderItems: products,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+      })
+    );
+
+    console.log({
+      orderItems: products,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+  };
 
   return (
-    <PlaceOrderStyled>
-      <Details>
-        <div>
-          <h2>Shipping</h2>
-          <p>
-            Address:{' '}
-            <span>
-              {shippingAddress.address}, {shippingAddress.city},{' '}
-              {shippingAddress.postalCode}, {shippingAddress.country}
-            </span>
-          </p>
-        </div>
-        <div>
-          <h2>Payment method</h2>
-          <p>
-            Method: <span>{paymentMethod}</span>
-          </p>
-        </div>
-        <div>
-          <h2>Order items</h2>
-          <OrderList>
-            {products.map((product: any) => (
-              <OrderItem>
-                <OrderImage
-                  onClick={() => history.push(`/product/${product.product}`)}
-                >
-                  <img src={product.image} alt={product.name} />
-                </OrderImage>
-                <OrderName>
-                  <p
+    <Spin spinning={loading}>
+      <PlaceOrderStyled>
+        <Details>
+          <div>
+            <h2>Shipping</h2>
+            <p>
+              Address:{' '}
+              <span>
+                {shippingAddress.address}, {shippingAddress.city},{' '}
+                {shippingAddress.postalCode}, {shippingAddress.country}
+              </span>
+            </p>
+          </div>
+          <div>
+            <h2>Payment method</h2>
+            <p>
+              Method: <span>{paymentMethod}</span>
+            </p>
+          </div>
+          <div>
+            <h2>Order items</h2>
+            <OrderList>
+              {products.map((product: any) => (
+                <OrderItem key={product.product}>
+                  <OrderImage
                     onClick={() => history.push(`/product/${product.product}`)}
                   >
-                    {product.name}
-                  </p>
-                </OrderName>
-                <OrderCant>
-                  <p>
-                    {product.qty} x ${product.price} = $
-                    {product.qty * product.price}
-                  </p>
-                </OrderCant>
-              </OrderItem>
-            ))}
-          </OrderList>
-        </div>
-      </Details>
+                    <img src={product.image} alt={product.name} />
+                  </OrderImage>
+                  <OrderName>
+                    <p
+                      onClick={() =>
+                        history.push(`/product/${product.product}`)
+                      }
+                    >
+                      {product.name}
+                    </p>
+                  </OrderName>
+                  <OrderCant>
+                    <p>
+                      {product.qty} x ${product.price} = $
+                      {product.qty * product.price}
+                    </p>
+                  </OrderCant>
+                </OrderItem>
+              ))}
+            </OrderList>
+          </div>
+        </Details>
 
-      <OrderSummary>
-        <div>
-          <h2>Order Summary</h2>
-        </div>
-        <div>
-          <span>Items:</span>
-          <span>${itemsPrice.toFixed(2)}</span>
-        </div>
-        <div>
-          <span>Shipping:</span>
-          <span>${shippingPrice.toFixed(2)}</span>
-        </div>
-        <div>
-          <span>Tax:</span>
-          <span>${taxPrice.toFixed(2)}</span>
-        </div>
-        <div>
-          <span>Total:</span>
-          <span>${totalPrice}</span>
-        </div>
-        <div>
-          <Button width="100%">Place order</Button>
-        </div>
-      </OrderSummary>
-    </PlaceOrderStyled>
+        <OrderSummary>
+          <div>
+            <h2>Order Summary</h2>
+          </div>
+          <div>
+            <span>Items:</span>
+            <span>${itemsPrice.toFixed(2)}</span>
+          </div>
+          <div>
+            <span>Shipping:</span>
+            <span>${shippingPrice.toFixed(2)}</span>
+          </div>
+          <div>
+            <span>Tax:</span>
+            <span>${taxPrice.toFixed(2)}</span>
+          </div>
+          <div>
+            <span>Total:</span>
+            <span>${totalPrice}</span>
+          </div>
+          <div>
+            <Button
+              width="100%"
+              onClick={placeOrderHanlder}
+              disabled={products.length === 0}
+            >
+              Place order
+            </Button>
+          </div>
+          {error && <Alert message={error} type="error" showIcon banner />}
+        </OrderSummary>
+      </PlaceOrderStyled>
+    </Spin>
   );
 };
 
