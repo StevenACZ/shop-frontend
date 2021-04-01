63bcc96d9187f49f79d7bf60cfcfa2410649ec50
stevenacz <scoailazaa@gmail.com>
2021-04-01 12:02:17 -0500

    improved

--- src/slices/order.ts
+++ src/slices/order.ts
@@ -19,6 +19,7 @@ export const orderSlice = createSlice({
   name: 'order',
   initialState,
   reducers: {
+    // CREATE
     orderCreateRequest: (state) => {
       state.order = null;
       state.error = null;
@@ -37,6 +38,26 @@ export const orderSlice = createSlice({
       state.success = false;
       state.loading = false;
     },
+    // DETAILS
+    orderDetailsRequest: (state) => {
+      state.order = null;
+      state.error = null;
+      state.success = false;
+      state.loading = true;
+    },
+    orderDetailsSuccess: (state, action) => {
+      state.order = action.payload;
+      state.error = null;
+      state.success = true;
+      state.loading = false;
+    },
+    orderDetailsFail: (state, action) => {
+      state.order = null;
+      state.error = action.payload;
+      state.success = false;
+      state.loading = false;
+    },
+    // CLEAR
     clearOrder: (state) => {
       state.order = null;
       state.error = null;
@@ -50,6 +71,9 @@ export const {
   orderCreateRequest,
   orderCreateSuccess,
   orderCreateFail,
+  orderDetailsRequest,
+  orderDetailsSuccess,
+  orderDetailsFail,
   clearOrder,
 } = orderSlice.actions;
 
