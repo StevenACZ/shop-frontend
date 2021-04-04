c5343ea76f5d5bfd31164deddde83dee5ee4201a
stevenacz <scoailazaa@gmail.com>
2021-04-04 15:48:15 -0500

    improved

--- src/pages/my-orders/Styles.ts
+++ src/pages/my-orders/Styles.ts
@@ -1,11 +1,8 @@
 import styled from 'styled-components';
 
 export const MyOrdersScreenStyled = styled.section`
-  padding: 20px 0;
-
   & > h2 {
     margin-bottom: 20px;
     text-transform: uppercase;
-    font-weight: bold;
   }
 `;
