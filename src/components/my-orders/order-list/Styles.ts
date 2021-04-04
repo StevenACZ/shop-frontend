4580d7b81cbe0863962e9aaa2666bab4815e09c1
stevenacz <scoailazaa@gmail.com>
2021-04-04 15:41:57 -0500

    improved

--- src/components/my-orders/order-list/Styles.ts
+++ src/components/my-orders/order-list/Styles.ts
@@ -1,11 +1,10 @@
 import styled from 'styled-components';
 
 export const OrderListStyled = styled.ul`
-  /* width: 100%;
+  width: 100%;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   grid-auto-rows: auto;
   grid-column-gap: 20px;
-  grid-row-gap: 30px; */
-  color: black;
+  grid-row-gap: 30px;
 `;
