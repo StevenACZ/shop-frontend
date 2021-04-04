c5a7557335f2f562d82bf91466c433360a5187f2
stevenacz <scoailazaa@gmail.com>
2021-04-04 15:41:38 -0500

    improved

--- src/components/my-orders/order-list/order-list-item/OrderListItem.tsx
+++ src/components/my-orders/order-list/order-list-item/OrderListItem.tsx
@@ -4,18 +4,63 @@ import React from 'react';
 // React Router
 import { useHistory } from 'react-router';
 
+import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
+
 // Styles
-import { OrderListItemStyled } from './Styles';
+import { OrderListItemStyled, Header, Body, Footer } from './Styles';
 
-interface Props {}
+// Antd Components
+import { Alert } from 'antd';
 
-const OrderListItem: React.FC<Props> = () => {
+// Components
+import Button from '../../../button/Button';
+
+interface Props {
+  _id: string;
+  createdAt: string;
+  totalPrice: number;
+  isPaid: boolean;
+  isDelivered: boolean;
+}
+
+const OrderListItem: React.FC<Props> = ({
+  _id,
+  createdAt,
+  totalPrice,
+  isPaid,
+  isDelivered,
+}) => {
   // History
   const history = useHistory();
 
   return (
     <OrderListItemStyled>
-      <p>sadsad</p>
+      <Header>
+        <h3>{_id && _id}</h3>
+      </Header>
+      <Body>
+        <p>
+          <span>Date:</span> {createdAt && createdAt.substring(0, 10)}
+        </p>
+        <p>
+          <span>Total Price:</span> ${totalPrice && totalPrice}
+        </p>
+      </Body>
+      <Footer>
+        {isPaid && isPaid ? (
+          <Alert message="Paid" type="success" showIcon />
+        ) : (
+          <Alert message="Not paid" type="error" showIcon />
+        )}
+        {isDelivered && isDelivered ? (
+          <Alert message="Delivered" type="success" showIcon />
+        ) : (
+          <Alert message="Not delivered" type="error" showIcon />
+        )}
+      </Footer>
+      <Button width="100%" onClick={() => history.push(`order/${_id}`)}>
+        Details
+      </Button>
     </OrderListItemStyled>
   );
 };
