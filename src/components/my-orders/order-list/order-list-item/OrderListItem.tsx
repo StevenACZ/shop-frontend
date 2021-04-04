96fc236cd062a033b1a9767f5935f38a0deb9339
stevenacz <scoailazaa@gmail.com>
2021-04-04 12:05:01 -0500

    added

---
+++ src/components/my-orders/order-list/order-list-item/OrderListItem.tsx
@@ -0,0 +1,13 @@
+// React
+import React from 'react';
+
+// Styles
+import { OrderListItemStyled } from './Styles';
+
+interface Props {}
+
+const OrderListItem: React.FC<Props> = () => {
+  return <OrderListItemStyled>OrderListItem</OrderListItemStyled>;
+};
+
+export default OrderListItem;
