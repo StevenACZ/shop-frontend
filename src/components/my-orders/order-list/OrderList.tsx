bf129c7fe142778c26a915e7ef30502265f98657
stevenacz <scoailazaa@gmail.com>
2021-04-04 12:27:45 -0500

    improved

--- src/components/my-orders/order-list/OrderList.tsx
+++ src/components/my-orders/order-list/OrderList.tsx
@@ -1,8 +1,24 @@
 // React
-import React from 'react';
+import React, { useEffect } from 'react';
+
+// Redux
+import { useDispatch, useSelector } from 'react-redux';
+
+// Redux - Actions
+import { listMyOrders } from '../../../actions/order';
+
+// Redux - Slices
+import {
+  selectError,
+  selectLoading,
+  selectOrders,
+} from '../../../slices/order';
 
 // Styles
 import { OrderListStyled } from './Styles';
+
+// Antd Components
+import { Alert, Spin } from 'antd';
 
 // Components
 import OrderListItem from './order-list-item/OrderListItem';
@@ -10,10 +26,30 @@ import OrderListItem from './order-list-item/OrderListItem';
 interface Props {}
 
 const OrderList: React.FC<Props> = () => {
+  // Dispatch
+  const dispatch = useDispatch();
+
+  // Selector
+  const loading = useSelector(selectLoading);
+  const orders = useSelector(selectOrders);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(listMyOrders());
+  }, [dispatch]);
+
   return (
-    <OrderListStyled>
-      <OrderListItem />
-    </OrderListStyled>
+    <Spin spinning={loading}>
+      <OrderListStyled>
+        {orders &&
+          orders.map((order: any) => (
+            <OrderListItem key={order._id} {...order} />
+          ))}
+      </OrderListStyled>
+      {orders && error && (
+        <Alert message={error} type="error" showIcon banner />
+      )}
+    </Spin>
   );
 };
 
