445f9a76d840487af52373bc1d7d5dcb4285b885
stevenacz <scoailazaa@gmail.com>
2021-04-07 13:26:58 -0500

    header added

--- src/pages/product/Styles.ts
+++ src/pages/product/Styles.ts
@@ -1,8 +1,19 @@
 import styled from 'styled-components';
 
-export const ProductScreenStyled = styled.section`
-  & > h1 {
-    margin-bottom: 20px;
+export const ProductScreenStyled = styled.section``;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+
+  & > h2 {
     text-transform: uppercase;
+  }
+
+  & > button {
+    width: auto;
+    padding: 0 20px;
   }
 `;
