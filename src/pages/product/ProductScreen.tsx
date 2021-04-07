90275bfa478d825f073f97b1d835031f091c7467
stevenacz <scoailazaa@gmail.com>
2021-04-07 14:01:17 -0500

    improved

--- src/pages/product/ProductScreen.tsx
+++ src/pages/product/ProductScreen.tsx
@@ -1,27 +1,8 @@
 // React
-import React, { useEffect } from 'react';
-
-// Redux
-import { useDispatch, useSelector } from 'react-redux';
-
-// Redux - Actions
-import { listProductDetails } from '../../actions/productDetails';
-
-// Redux - Slices
-import {
-  selectError,
-  selectLoading,
-  selectProduct,
-} from '../../slices/productDetails';
-
-// React Router
-import { useHistory, useParams } from 'react-router';
+import React from 'react';
 
 // Styles
 import { ProductScreenStyled, Header } from './Styles';
-
-// Antd Components
-import { Alert, Button, Spin } from 'antd';
 
 // Components
 import ProductDetails from '../../components/product/product-details/ProductDetails';
@@ -29,48 +10,12 @@ import ProductDetails from '../../components/product/product-details/ProductDeta
 interface Props {}
 
 const ProductScreen: React.FC<Props> = () => {
-  // History
-  const history = useHistory();
-
-  // Dispatch
-  const dispatch = useDispatch();
-
-  // Selector
-  const product = useSelector(selectProduct);
-  const loading = useSelector(selectLoading);
-  const error = useSelector(selectError);
-
-  const { productID } = useParams() as { productID: string };
-
-  useEffect(() => {
-    dispatch(listProductDetails(productID));
-  }, [dispatch, productID]);
-
   return (
     <ProductScreenStyled>
       <Header>
         <h2>Product Details</h2>
       </Header>
-      <Spin spinning={loading}>
-        {product && <ProductDetails {...product} />}
-        {error && (
-          <Alert
-            message={error}
-            type="error"
-            showIcon
-            banner
-            action={
-              <Button
-                size="small"
-                type="primary"
-                onClick={() => history.push('/')}
-              >
-                Go Home
-              </Button>
-            }
-          />
-        )}
-      </Spin>
+      <ProductDetails />
     </ProductScreenStyled>
   );
 };
