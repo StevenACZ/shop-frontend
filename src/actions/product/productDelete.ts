d48505e4d5fdfbe1a231d05b4c85c975c36a3bf1
stevenacz <scoailazaa@gmail.com>
2021-04-08 16:52:34 -0500

    improved

---
+++ src/actions/product/productDelete.ts
@@ -0,0 +1,42 @@
+// Axios
+import axios from '../../axios/index';
+
+// Redux - Slices
+import {
+  productDeleteRequest,
+  productDeleteSuccess,
+  productDeleteFail,
+} from '../../slices/productDelete';
+
+export const deleteProduct = (productId: string) => async (
+  dispatch: any,
+  getState: any
+) => {
+  try {
+    dispatch(productDeleteRequest());
+
+    const {
+      user: {
+        userInfo: { token },
+      },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    await axios.delete(`/api/products/${productId}`, config);
+
+    dispatch(productDeleteSuccess());
+  } catch (error) {
+    dispatch(
+      productDeleteFail(
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      )
+    );
+  }
+};
