fab06047c213e8c2b3b6d420165d3a4b3e3dcd52
stevenacz <scoailazaa@gmail.com>
2021-04-08 16:34:25 -0500

    improved

--- src/components/admin/products/product-list/product-list-item/ProductListItem.tsx
+++ src/components/admin/products/product-list/product-list-item/ProductListItem.tsx
@@ -2,10 +2,16 @@
 import React from 'react';
 
 // Redux
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 // Redux - Actions
-import { deleteUser } from '../../../../../actions/user';
+import { deleteProduct } from '../../../../../actions/productDelete';
+
+// Redux - Slices
+import {
+  selectLoading,
+  selectError,
+} from '../../../../../slices/productDelete';
 
 // React Router
 import { useHistory } from 'react-router';
@@ -20,7 +26,7 @@ import {
 } from './Styles';
 
 // Antd Components
-import { Alert, Popconfirm, message } from 'antd';
+import { Alert, Spin, Popconfirm } from 'antd';
 
 // Components
 import Button from '../../../../button/Button';
@@ -48,58 +54,65 @@ const ProductListItem: React.FC<Props> = ({
   // Dispatch
   const dispatch = useDispatch();
 
-  const handleDelete = (userId: string) => {
-    dispatch(deleteUser(userId));
-    message.success('User Deleted');
+  // Selector
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
+  const handleDelete = (productId: string) => {
+    dispatch(deleteProduct(productId));
   };
 
-  const handleGoToUpdate = (userId: string) => {
-    history.push(`/admin/${userId}/edit`);
+  const handleGoToUpdate = (productId: string) => {
+    history.push(`/admin/${productId}/edit`);
   };
 
   return (
-    <ProductListItemStyled>
-      <Header>
-        <h3>{_id}</h3>
-      </Header>
-      <Body>
-        <p>
-          <span>Name:</span> {name}
-        </p>
-        <p>
-          <span>Price:</span> ${price}
-        </p>
-        <p>
-          <span>Category:</span> {category}
-        </p>
-        <p>
-          <span>Brand:</span> {brand}
-        </p>
-        <p>
-          <span>Stock:</span> {countInStock}
-        </p>
-      </Body>
-      <Footer>
-        {countInStock === 0 ? (
-          <Alert message="Out of Stock" type="error" showIcon />
-        ) : (
-          <Alert message="In Stock" type="success" showIcon />
-        )}
-      </Footer>
-      <ActionsContainer>
-        <Button width="48%" onClick={() => handleGoToUpdate(_id)}>
-          Edit
-        </Button>
-        <Popconfirm
-          title="Are you sure to delete this user?"
-          onConfirm={() => handleDelete(_id)}
-          okText="Yes"
-          cancelText="No"
-        >
-          <Button width="48%">Delete</Button>
-        </Popconfirm>
-      </ActionsContainer>
-    </ProductListItemStyled>
+    <Spin spinning={loading}>
+      <ProductListItemStyled>
+        <Header>
+          <h3>{_id}</h3>
+        </Header>
+        <Body>
+          <p>
+            <span>Name:</span> {name}
+          </p>
+          <p>
+            <span>Price:</span> ${price}
+          </p>
+          <p>
+            <span>Category:</span> {category}
+          </p>
+          <p>
+            <span>Brand:</span> {brand}
+          </p>
+          <p>
+            <span>Stock:</span> {countInStock}
+          </p>
+        </Body>
+        <Footer>
+          {countInStock === 0 ? (
+            <Alert message="Out of Stock" type="error" showIcon />
+          ) : (
+            <Alert message="In Stock" type="success" showIcon />
+          )}
+        </Footer>
+        <ActionsContainer>
+          <Button width="48%" onClick={() => handleGoToUpdate(_id)}>
+            Edit
+          </Button>
+          <Popconfirm
+            title="Are you sure to delete this product?"
+            onConfirm={() => handleDelete(_id)}
+            okText="Yes"
+            cancelText="No"
+          >
+            <Button width="48%">Delete</Button>
+          </Popconfirm>
+        </ActionsContainer>
+      </ProductListItemStyled>
+
+      {error && <Alert message={error} type="error" showIcon banner />}
+    </Spin>
   );
 };
 
