8bf9e331d8c75c6d45b5df80338be169a744d6c2
stevenacz <scoailazaa@gmail.com>
2021-04-08 16:35:10 -0500

    improved

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -7,11 +7,13 @@ import { productDetailsSlice } from '../slices/productDetails';
 import { cartSlice } from '../slices/cart';
 import { userSlice } from '../slices/user';
 import { orderSlice } from '../slices/order';
+import { productDeleteSlice } from '../slices/productDelete';
 
 const store = configureStore({
   reducer: {
     productList: productListSlice.reducer,
     productDetails: productDetailsSlice.reducer,
+    productDelete: productDeleteSlice.reducer,
     cart: cartSlice.reducer,
     user: userSlice.reducer,
     order: orderSlice.reducer,
