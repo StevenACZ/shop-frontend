ff859b593a16202d04948b9416ef9aff847ac920
stevenacz <scoailazaa@gmail.com>
2021-04-10 20:30:18 -0500

    added

---
+++ src/components/admin/orders/order-list/order-list-item/OrderListItem.tsx
@@ -0,0 +1,78 @@
+// React
+import React from 'react';
+
+// React Router
+import { useHistory } from 'react-router';
+
+// Styles
+import {
+  OrderListItemStyled,
+  Header,
+  Body,
+  Footer,
+  ActionsContainer,
+} from './Styles';
+
+// Antd Components
+import { Alert } from 'antd';
+
+// Components
+import Button from '../../../../button/Button';
+
+interface Props {
+  _id: string;
+  createdAt: string;
+  totalPrice: number;
+  isPaid: boolean;
+  isDelivered: boolean;
+}
+
+const OrderListItem: React.FC<Props> = ({
+  _id,
+  createdAt,
+  totalPrice,
+  isPaid,
+  isDelivered,
+}) => {
+  // History
+  const history = useHistory();
+
+  const handleGoToOrder = (orderId: string) => {
+    history.push(`/order/${orderId}`);
+  };
+
+  return (
+    <OrderListItemStyled>
+      <Header>
+        <h3>{_id}</h3>
+      </Header>
+      <Body>
+        <p>
+          <span>Date:</span> {createdAt.substring(0, 10)}
+        </p>
+        <p>
+          <span>Total Price:</span> ${totalPrice}
+        </p>
+      </Body>
+      <Footer>
+        {isPaid ? (
+          <Alert message="Paid" type="success" showIcon />
+        ) : (
+          <Alert message="Not paid" type="error" showIcon />
+        )}
+        {isDelivered ? (
+          <Alert message="Delivered" type="success" showIcon />
+        ) : (
+          <Alert message="Not delivered" type="error" showIcon />
+        )}
+      </Footer>
+      <ActionsContainer>
+        <Button width="100%" onClick={() => handleGoToOrder(_id)}>
+          Details
+        </Button>
+      </ActionsContainer>
+    </OrderListItemStyled>
+  );
+};
+
+export default OrderListItem;
