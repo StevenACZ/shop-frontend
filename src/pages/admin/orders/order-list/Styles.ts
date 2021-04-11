8752d18ff49f798e3ebc2eb003d910f5dce893ae
stevenacz <scoailazaa@gmail.com>
2021-04-10 19:51:55 -0500

    added

---
+++ src/pages/admin/orders/order-list/Styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const OrderListScreenStyled = styled.section``;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+
+  & > h2 {
+    text-transform: uppercase;
+  }
+`;
