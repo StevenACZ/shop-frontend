43ff0f2ced0c12c11ea2939b6fbc2357379b4fc6
stevenacz <scoailazaa@gmail.com>
2021-04-12 16:29:28 -0500

    improved

--- src/pages/admin/orders/order-list/OrderListScreen.tsx
+++ src/pages/admin/orders/order-list/OrderListScreen.tsx
@@ -2,11 +2,10 @@
 import React, { useEffect } from 'react';
 
 // Redux
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 // Redux - Slices
 import { selectUserInfo } from '../../../../slices/user';
-import { orderListReset } from '../../../../slices/order/orderList';
 
 // React Router
 import { useHistory } from 'react-router';
@@ -22,9 +21,6 @@ interface Props {}
 const OrderListScreen: React.FC<Props> = () => {
   // History
   const history = useHistory();
-
-  // Dispatch
-  const dispatch = useDispatch();
 
   // Selector
   const userInfo = useSelector(selectUserInfo) as {
@@ -42,12 +38,6 @@ const OrderListScreen: React.FC<Props> = () => {
       }
     }
   }, [history, userInfo]);
-
-  useEffect(() => {
-    return () => {
-      dispatch(orderListReset());
-    };
-  }, [dispatch]);
 
   return (
     <OrderListScreenStyled>
