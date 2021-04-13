ad53a110d5f53d6bb8a4ef6b472e872a2e610b7a
stevenacz <scoailazaa@gmail.com>
2021-04-13 17:12:59 -0500

    improved

--- src/actions/product/productList.ts
+++ src/actions/product/productList.ts
@@ -8,11 +8,11 @@ import {
   productListFail,
 } from '../../slices/product/productList';
 
-export const listProducts = () => async (dispatch: any) => {
+export const listProducts = (keyword = '') => async (dispatch: any) => {
   try {
     dispatch(productListRequest());
 
-    const { data } = await axios.get('/api/products');
+    const { data } = await axios.get(`/api/products?keyword=${keyword}`);
 
     dispatch(productListSuccess(data));
   } catch (error) {
