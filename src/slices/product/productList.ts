3ef97cf2b40cbbcc875a5ddbabc4d1a269a1f108
stevenacz <scoailazaa@gmail.com>
2021-04-13 18:14:54 -0500

    improved

--- src/slices/product/productList.ts
+++ src/slices/product/productList.ts
@@ -3,12 +3,16 @@ import type { RootState } from '../../store/configureStore';
 
 interface ProductListState {
   products: null | [];
+  pages: null | number;
+  page: null | number;
   loading: boolean;
   error: null | string;
 }
 
 const initialState: ProductListState = {
   products: null,
+  pages: null,
+  page: null,
   loading: false,
   error: null,
 };
@@ -19,21 +23,29 @@ export const productListSlice = createSlice({
   reducers: {
     productListRequest: (state) => {
       state.products = null;
+      state.pages = null;
+      state.page = null;
       state.loading = true;
       state.error = null;
     },
     productListSuccess: (state, action) => {
-      state.products = action.payload;
+      state.products = action.payload.products;
+      state.pages = action.payload.pages;
+      state.page = action.payload.page;
       state.loading = false;
       state.error = null;
     },
     productListFail: (state, action) => {
       state.products = null;
+      state.pages = null;
+      state.page = null;
       state.loading = false;
       state.error = action.payload;
     },
     productListReset: (state) => {
       state.products = null;
+      state.pages = null;
+      state.page = null;
       state.loading = false;
       state.error = null;
     },
@@ -49,6 +61,10 @@ export const {
 
 export const selectProductListProducts = (state: RootState) =>
   state.productList.products;
+export const selectProductListPages = (state: RootState) =>
+  state.productList.pages;
+export const selectProductListPage = (state: RootState) =>
+  state.productList.page;
 export const selectProductListLoading = (state: RootState) =>
   state.productList.loading;
 
