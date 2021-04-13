4b993e0d6735ac0054a59d3320d6115f22f69d2f
stevenacz <scoailazaa@gmail.com>
2021-04-13 18:11:50 -0500

    improved

--- src/components/home/product-list/Styles.ts
+++ src/components/home/product-list/Styles.ts
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 
-export const ProductListStyled = styled.ul`
+export const ProductListStyled = styled.section`
+  & > .ant-pagination {
+    padding-top: 20px;
+  }
+`;
+
+export const ProductListContent = styled.ul`
   width: 100%;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
