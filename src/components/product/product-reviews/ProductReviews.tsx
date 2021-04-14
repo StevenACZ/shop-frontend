3a21d67709a9e611669813737fa87d47ecf78e4f
stevenacz <scoailazaa@gmail.com>
2021-04-14 16:31:52 -0500

    improved

--- src/components/product/product-reviews/ProductReviews.tsx
+++ src/components/product/product-reviews/ProductReviews.tsx
@@ -2,12 +2,23 @@
 import React from 'react';
 
 // Styles
-import { ProductReviewsStyled } from './Styles';
+import { ProductReviewsStyled, Header } from './Styles';
+
+// Components
+import ReviewList from './review-list/ReviewList';
 
 interface Props {}
 
 const ProductReviews: React.FC<Props> = () => {
-  return <ProductReviewsStyled>ProductReviews</ProductReviewsStyled>;
+  return (
+    <ProductReviewsStyled>
+      <Header>
+        <h2>Reviews</h2>
+      </Header>
+      <ReviewList />
+      {/* <AddNewReview/> */}
+    </ProductReviewsStyled>
+  );
 };
 
 export default ProductReviews;
