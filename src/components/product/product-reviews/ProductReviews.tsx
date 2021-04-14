cd3e7d2d1ca752d51be83a82318e761b4b6a34ea
stevenacz <scoailazaa@gmail.com>
2021-04-14 16:01:02 -0500

    productReview added

---
+++ src/components/product/product-reviews/ProductReviews.tsx
@@ -0,0 +1,13 @@
+// React
+import React from 'react';
+
+// Styles
+import { ProductReviewsStyled } from './Styles';
+
+interface Props {}
+
+const ProductReviews: React.FC<Props> = () => {
+  return <ProductReviewsStyled>ProductReviews</ProductReviewsStyled>;
+};
+
+export default ProductReviews;
