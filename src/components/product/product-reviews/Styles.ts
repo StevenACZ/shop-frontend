e992b541dbb10763a18e4aa5112c3f5af491b8b6
stevenacz <scoailazaa@gmail.com>
2021-04-14 16:31:57 -0500

    improved

--- src/components/product/product-reviews/Styles.ts
+++ src/components/product/product-reviews/Styles.ts
@@ -1,100 +1,13 @@
 import styled from 'styled-components';
 
-export const ProductReviewsStyled = styled.section``;
+export const ProductReviewsStyled = styled.section`
+  margin: 40px 0;
+`;
 
 export const Header = styled.div`
-  margin-bottom: 15px;
-`;
+  margin-bottom: 20px;
 
-export const Main = styled.div`
-  display: grid;
-  grid-template-columns: 35% 1fr 1fr;
-  column-gap: 40px;
-
-  @media (max-width: 1000px) {
-  }
-
-  @media (max-width: 768px) {
-    grid-template-columns: 1fr;
-    column-gap: 0;
-    row-gap: 30px;
-  }
-`;
-
-export const Image = styled.div`
-  & > img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-`;
-
-export const Description = styled.div`
-  & > div {
-    padding: 10px 20px;
-  }
-
-  & > div:nth-child(1) {
-    font-size: 18px;
-    line-height: 35px;
-    padding: 0 20px 20px;
-  }
-
-  & > div:nth-child(2) {
-    border-top: 1px solid black;
-    border-bottom: 1px solid black;
-    display: flex;
-    align-items: center;
-
-    & > ul {
-      font-size: 15px;
-    }
-
-    & > p {
-      margin-left: 15px;
-    }
-  }
-
-  & > div:nth-child(3) {
-    border-bottom: 1px solid black;
-    font-size: 14px;
-  }
-
-  & > div:nth-child(4) {
-    font-size: 14px;
-  }
-`;
-
-export const AddToCart = styled.div`
-  & > div {
-    padding: 10px 20px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  & > div:nth-child(1) {
-    border-top: 1px solid black;
-    border-left: 1px solid black;
-    border-right: 1px solid black;
-    border-bottom: 1px solid black;
-  }
-
-  & > div:nth-child(2) {
-    border-left: 1px solid black;
-    border-right: 1px solid black;
-    border-bottom: 1px solid black;
-  }
-
-  & > div:nth-child(3) {
-    border-left: 1px solid black;
-    border-right: 1px solid black;
-    border-bottom: 1px solid black;
-  }
-
-  & > div:nth-child(4) {
-    border-left: 1px solid black;
-    border-right: 1px solid black;
-    border-bottom: 1px solid black;
+  & > h2 {
+    text-transform: uppercase;
   }
 `;
