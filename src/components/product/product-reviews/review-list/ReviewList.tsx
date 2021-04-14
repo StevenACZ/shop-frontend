e17a89139c7b488f7287d68c5609566c2a5983a3
stevenacz <scoailazaa@gmail.com>
2021-04-14 17:09:26 -0500

    improved

---
+++ src/components/product/product-reviews/review-list/ReviewList.tsx
@@ -0,0 +1,45 @@
+// React
+import React from 'react';
+
+// Redux
+import { useSelector } from 'react-redux';
+
+// Redux - Slices
+import {
+  selectProductDetailsProduct,
+  selectProductDetailsLoading,
+  selectProductDetailsError,
+} from '../../../../slices/product/productDetails';
+
+// Styles
+import { ReviewListStyled } from './Styles';
+
+// Antd Components
+import { Alert, Spin } from 'antd';
+
+// Components
+import ReviewListItem from './review-list-item/ReviewListItem';
+
+interface Props {}
+
+const ReviewList: React.FC<Props> = () => {
+  // Selector
+  const product = useSelector(selectProductDetailsProduct) as any;
+  const loading = useSelector(selectProductDetailsLoading);
+  const error = useSelector(selectProductDetailsError);
+
+  return (
+    <Spin spinning={loading}>
+      <ReviewListStyled>
+        {product &&
+          product.reviews.map((review: any) => <ReviewListItem {...review} />)}
+      </ReviewListStyled>
+      {product && product.reviews.length === 0 && (
+        <Alert message="No Reviews" type="warning" />
+      )}
+      {error && <Alert message={error} type="error" showIcon banner />}
+    </Spin>
+  );
+};
+
+export default ReviewList;
