41ed27df1b4bdc0b5f34c71d8233569d36f3ad12
stevenacz <scoailazaa@gmail.com>
2021-04-14 21:45:46 -0500

    improved

--- src/components/product/product-reviews/Styles.ts
+++ src/components/product/product-reviews/Styles.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
 export const ProductReviewsStyled = styled.section`
-  margin: 40px 0;
+  margin: 40px 0 0 0;
+  width: 40%;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
