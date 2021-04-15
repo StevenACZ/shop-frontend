6b9d7143156198609b7c9200e1f5486166998787
stevenacz <scoailazaa@gmail.com>
2021-04-14 20:47:10 -0500

    added

---
+++ src/components/product/product-reviews/add-new-review/Styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const AddNewReviewStyled = styled.section`
+  margin: 40px 0;
+`;
+
+export const Header = styled.div`
+  margin-bottom: 20px;
+
+  & > h2 {
+    text-transform: uppercase;
+  }
+`;
+
+export const Form = styled.form``;
