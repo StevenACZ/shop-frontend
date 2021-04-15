724fe76da00e1960105ce4d24718a7cd4f45321c
stevenacz <scoailazaa@gmail.com>
2021-04-14 20:46:04 -0500

    improved

--- src/components/product/product-reviews/Styles.ts
+++ src/components/product/product-reviews/Styles.ts
@@ -3,11 +3,3 @@ import styled from 'styled-components';
 export const ProductReviewsStyled = styled.section`
   margin: 40px 0;
 `;
-
-export const Header = styled.div`
-  margin-bottom: 20px;
-
-  & > h2 {
-    text-transform: uppercase;
-  }
-`;
