e87e3a045cd24b80360c31ee4d46636735448c40
stevenacz <scoailazaa@gmail.com>
2021-04-14 21:45:26 -0500

    improved

--- src/components/product/product-reviews/add-new-review/Styles.ts
+++ src/components/product/product-reviews/add-new-review/Styles.ts
@@ -1,15 +1,19 @@
 import styled from 'styled-components';
 
 export const AddNewReviewStyled = styled.section`
-  margin: 40px 0;
+  margin: 40px 0 0 0;
 `;
 
 export const Header = styled.div`
-  margin-bottom: 20px;
+  margin-bottom: 10px;
 
   & > h2 {
     text-transform: uppercase;
   }
 `;
 
-export const Form = styled.form``;
+export const Form = styled.form`
+  & > .ant-rate {
+    margin-bottom: 12px;
+  }
+`;
