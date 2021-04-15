0b5eb3db9977165fdf74599d64f6e96e6ba36069
stevenacz <scoailazaa@gmail.com>
2021-04-14 20:45:11 -0500

    improved

--- src/components/product/product-reviews/review-list/Styles.ts
+++ src/components/product/product-reviews/review-list/Styles.ts
@@ -3,3 +3,11 @@ import styled from 'styled-components';
 export const ReviewListStyled = styled.ul`
   padding: 0 20px;
 `;
+
+export const Header = styled.div`
+  margin-bottom: 20px;
+
+  & > h2 {
+    text-transform: uppercase;
+  }
+`;
